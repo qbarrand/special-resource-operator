2a9902a5e65a8fcc222e0c167960e242422239fa
Quentin Barrand <quba@redhat.com>
2022-02-02 09:29:23 +0100

    Remove Kubernetes-related deployment files.

--- Makefile.helper.mk
+++ Makefile.helper.mk
@@ -7,8 +7,6 @@ PULLPOLICY       ?= IfNotPresent
 TAG              ?= $(shell git branch --show-current)
 CSPLIT           ?= csplit - --prefix="" --suppress-matched --suffix-format="%04d.yaml"  /---/ '{*}' --silent
 YAMLFILES        ?= $(shell  find manifests charts -name "*.yaml"  -not \( -path "charts/lustre/lustre-aws-fsx-0.0.1/csi-driver/*" -prune \)  -not \( -path "charts/*/shipwright-*/*" -prune \) -not \( -path "charts/experimental/*" -prune \) )
-PLATFORM         ?= ""
-SUFFIX           ?= $(shell if [ ${PLATFORM} == "k8s" ]; then echo "-${PLATFORM}"; fi)
 CONTAINER_COMMAND := $(or ${CONTAINER_COMMAND},podman)
 CLUSTER_CLIENT := $(or ${CLUSTER_CLIENT},kubectl)
 KUBECONFIG       ?= ${HOME}/.kube/config
